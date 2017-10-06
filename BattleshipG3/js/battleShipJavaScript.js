11cc6e9b5f6ec910dcc9519a5de90a47f667fb50
Alexander Grahn <mralex94@hotmail.com>
2017-10-06 10:42:03 +0200

    javascript exempel

--- BattleshipG3/js/battleShipJavaScript.js
+++ BattleshipG3/js/battleShipJavaScript.js
@@ -1,4 +1,11 @@
 ﻿$(document).ready(function () {
-
+    //var input = "<div class='tableRow'>" +
+    //    " <div class='tableCell'>" + $("#firstname").val() + "</div>" +
+    //    " <div class='tableCell'>" + $("#lastname").val() + "</div>" +
+    //    " <div class='tableCell'>" + $("#datepicker").val() + "</div>" +
+    //    " <div class='tableCell'>" + sex + "</div>" +
+    //    " <div class='tableCell'>" + age + "</div>" +
+    //    " <div class='tableCell'>" + birthday + "</div>" +
+    //    " </div>";
 
 });
